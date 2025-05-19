9c68d0a39123bb27f95b3eb1b40cb916b2f629a1
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-20 01:11:27 +0900

    refactor #9: api 코드 구조 수정

--- src/api/studyGroup/postCreateStudyGroup.js
+++ src/api/studyGroup/postCreateStudyGroup.js
@@ -1,22 +1,23 @@
-async function postCreateStudyGroup(studyData) {
-  return fetch("http://localhost:8080/study-group/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(studyData),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        console.log("백엔드 통신 에러");
-        throw new Error("스터디 생성에 실패했습니다.");
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.error("스터디 생성 중 오류 발생:", error);
-      throw error;
+const API_BASE_URL = "http://localhost:8080";
+
+const postCreateStudyGroup = async (studyData) => {
+  const url = `${API_BASE_URL}/study-group/create?userId=1`; //테스트용 url
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(studyData),
     });
-}
+
+    const data = await res.json();
+    return { ok: res.ok, data };
+  } catch (error) {
+    console.error("스터디 생성 중 오류 발생:", error);
+    return { ok: false, data: null };
+  }
+};
 
 export default postCreateStudyGroup;
