88bea634d4e738a75a255fcfaa253e0e09befb06
seohee-P <p03061180@gmail.com>
2025-05-20 17:10:02 +0900

    feat #6: 게시글 작성 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import BoardInfo from "./pages/board/BoardInfo.jsx";
+import WriteBoard from "./components/board/WriteBoard.jsx";
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path={`/board/:boardId`} element={<BoardInfo/>}/>
+                <Route path="/board/create" element={<WriteBoard />}/>
             </Routes>
         </>
     )
