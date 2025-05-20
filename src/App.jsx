a120050edba7fbbab72d54eea44e8e47a8778f6f
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-20 10:43:13 +0900

    fix #3: app.jsx 충돌 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudyGroupList from "./pages/studyGroup/StudyGroupList";
 import CreateStudyGroup from "./pages/studyGroup/CreateStudyGroup";
 import StudyGroupDetail from "./pages/studyGroup/StudyGroupDetail";
+import BoardInfo from "./pages/board/BoardInfo.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/study-group" element={<StudyGroupList />} />
         <Route path="/study-group/create" element={<CreateStudyGroup />} />
         <Route path="/study-group/detail" element={<StudyGroupDetail />} />
+        <Route path="/board/:boardId" element={<BoardInfo />} />
       </Routes>
     </BrowserRouter>
   );
