f5e02805659f6cd8855d5c19525c98191062a6f8
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-22 02:10:16 +0900

    feat #24: 이미 신청한 스터디에 관한 alert 문구 추가

--- src/api/studyGroup/joinStudy.js
+++ src/api/studyGroup/joinStudy.js
@@ -5,12 +5,19 @@ const joinStudy = async (studyId, userId = 1) => {
       { method: "POST" }
     );
 
-    if (!res.ok) throw new Error("신청 실패");
     const msg = await res.text();
+
+    if (!res.ok) {
+      if (msg.includes("이미 신청한 유저입니다.")) {
+        alert("이미 신청한 스터디입니다.");
+      }
+      return;
+    }
+
     alert(msg || "신청이 완료되었습니다!");
   } catch (err) {
     console.error("스터디 신청 오류:", err);
-    alert("신청 중 문제가 발생했습니다.");
+    alert("네트워크 오류가 발생했습니다. 잠시 후 다시 시도해주세요. ");
   }
 };
 
