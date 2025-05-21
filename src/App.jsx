da5c7aa7ff802980e481ede257cc0e0eb9544844
dohy0103 <kdohy0103@gmail.com>
2025-05-21 16:43:11 +0900

    게시글 목록 페이징 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import BoardInfo from "./pages/board/BoardInfo.jsx";
+import BoardList from "./pages/board/BoardList.jsx";
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path={`/board/:boardId`} element={<BoardInfo/>}/>
+                <Route path={`/boards`} element={<BoardList />} />
+                <Route path={`/boards/:boardId`} element={<BoardInfo />} />
             </Routes>
         </>
     )
 }
 
-export default App
+export default App;
