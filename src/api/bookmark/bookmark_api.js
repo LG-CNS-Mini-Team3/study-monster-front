e15ce8a3b4c8f2cd40edbbf4904c77137e50d1fd
kaebalsaebal <56146987+kaebalsaebal@users.noreply.github.com>
2025-05-22 11:18:48 +0900

    feat: 북마크 콤포넌트 구현

---
+++ src/api/bookmark/bookmark_api.js
@@ -0,0 +1,60 @@
+const API_BASE_URL = "http://localhost:8080/bookmark";
+
+// 북마크 여부 확인
+export const checkBookmark = async (body) => {
+  try {
+    const res = await fetch(`${API_BASE_URL}/check`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      throw new Error("북마크 확인 실패");
+    }
+    return await res.json(); // true 또는 false
+  } catch (err) {
+    console.error("북마크 확인 에러:", err);
+    return false;
+  }
+};
+
+// 북마크 추가
+export const addBookmark = async (body) => {
+  try {
+    const res = await fetch(`${API_BASE_URL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) throw new Error("북마크 추가 실패");
+    return true;
+  } catch (err) {
+    console.error("북마크 추가 에러:", err);
+    return false;
+  }
+};
+
+// 북마크 해제
+export const removeBookmark = async (body) => {
+  try {
+    const res = await fetch(`${API_BASE_URL}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) throw new Error("북마크 해제 실패");
+    return true;
+  } catch (err) {
+    console.error("북마크 해제 에러:", err);
+    return false;
+  }
+};
