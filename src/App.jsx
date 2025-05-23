59c06202eb555abcbb41e7b23dc42288cc71595f
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-23 14:53:01 +0900

    refactor: app.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import BoardInfo from "./pages/board/BoardInfo.jsx";
+import BoardList from "./pages/board/BoardList.jsx";
+import WriteBoard from "./components/board/WriteBoard.jsx";
+import EditBoard from "./components/board/EditBoard.jsx";
+import React from "react";
 import "./App.css";
 import StudyGroupList from "./pages/studyGroup/StudyGroupList";
 import CreateStudyGroup from "./pages/studyGroup/CreateStudyGroup";
@@ -7,9 +12,6 @@ import { Container } from "lucide-react";
 import Header from "./components/header/Header.jsx";
 import MyPage from "./components/auth/MyPage.jsx";
 import Home from "./components/HomPage.jsx";
-import EditBoard from "./components/board/EditBoard.jsx";
-import WriteBoard from "./components/board/WriteBoard.jsx";
-import BoardList from "./pages/board/BoardList.jsx";
 
 function App() {
   return (
@@ -23,9 +25,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/study-groups" element={<StudyGroupList />} />
         <Route path="/study-groups/new" element={<CreateStudyGroup />} />
-        <Route path={`/study-groups/:studyId`} element={<StudyGroupDetail />} />
+        <Route path={`/study-groups/:boardId`} element={<StudyGroupDetail />} />
         <Route path="/boards/new" element={<WriteBoard />} />
-        <Route path={`/boards`} element={<BoardList />} />
         <Route path={`/boards/:boardId`} element={<BoardInfo />} />
         <Route path={`/boards/:boardId/edit`} element={<EditBoard />} />
       </Routes>
