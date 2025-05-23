ca8969f6d9411d70949d278de8f4e139a3709dfa
kaebalsaebal <56146987+kaebalsaebal@users.noreply.github.com>
2025-05-23 14:06:52 +0900

    fix: 코드 꼬임 수정, feat: MyPage에 북마크리스트 추가, 로그인 연동을 위해 BoardHeader 및 BoardInfo 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,13 @@ import WriteBoard from "./components/board/WriteBoard.jsx";
 import EditBoard from "./components/board/EditBoard.jsx";
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import StudyGroupList from "./pages/studyGroup/StudyGroupList";
 import CreateStudyGroup from "./pages/studyGroup/CreateStudyGroup";
 import StudyGroupDetail from "./pages/studyGroup/StudyGroupDetail";
-import BoardInfo from "./pages/board/BoardInfo.jsx";
 import { Container } from "lucide-react";
 import Header from "./components/header/Header.jsx";
 import MyPage from "./components/auth/MyPage.jsx";
 import Home from "./components/HomPage.jsx";
-import EditBoard from "./components/board/EditBoard.jsx";
-import WriteBoard from "./components/board/WriteBoard.jsx";
 
 function App() {
   return (
