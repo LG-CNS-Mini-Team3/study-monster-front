c2a2f036a5605ec170bb98e7190b320fa58d9306
dohy0103 <kdohy0103@gmail.com>
2025-05-23 10:07:29 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import BoardList from "./pages/board/BoardList";
-import BoardDetail from "./pages/board/BoardDetail";
+import StudyGroupList from "./pages/studyGroup/StudyGroupList";
+import CreateStudyGroup from "./pages/studyGroup/CreateStudyGroup";
+import StudyGroupDetail from "./pages/studyGroup/StudyGroupDetail";
+import BoardInfo from "./pages/board/BoardInfo.jsx";
+import { Container } from "lucide-react";
+import Header from "./components/header/Header.jsx";
+import MyPage from "./components/auth/MyPage.jsx";
+import Home from "./components/HomPage.jsx";
+import EditBoard from "./components/board/EditBoard.jsx";
+import WriteBoard from "./components/board/WriteBoard.jsx";
+import BoardList from "./pages/board/BoardList.jsx";
+
 function App() {
   return (
     <>
+      <Header>
+        <Container style={{ minHeight: "75vh" }}>studyMonster</Container>
+      </Header>
+      <div style={{ paddingTop: "50px" }}></div>
       <Routes>
+        <Route path="/auth/MyPage" element={<MyPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/study-groups" element={<StudyGroupList />} />
+        <Route path="/study-groups/new" element={<CreateStudyGroup />} />
+        <Route path={`/study-groups/:studyId`} element={<StudyGroupDetail />} />
+        <Route path="/boards/new" element={<WriteBoard />} />
         <Route path={`/boards`} element={<BoardList />} />
-        <Route path={`/boards/:boardId`} element={<BoardDetail />} />
+        <Route path={`/boards/:boardId`} element={<BoardInfo />} />
+        <Route path={`/boards/:boardId/edit`} element={<EditBoard />} />
       </Routes>
     </>
   );
